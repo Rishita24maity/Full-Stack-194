438ec6f04abd92090e77ad9f06f67d51bb19ce82
Rishita24maity <rishitamaity02@gmail.com>
2025-02-22 23:47:58 +0530

    script

---
+++ Day 9/Classwork/index.js
@@ -0,0 +1,39 @@
+// {
+//     let a=10;
+//     console.log(a)
+// }
+
+//+,-,*,/,%,** :arithmatic
+//==,===,!=,!==,>,<,>=,<= :relational
+// &&;||,! :logical
+//=,+=,-=,*=,/= :assignment
+
+
+// let a=10,b="10"
+// console.log(typeof(a))
+// console.log(typeof(b))
+
+// b=+b
+// if(a===+b){
+//     console.log("a is equal to b")
+// }else{
+//     console.log("a is not equal to b")
+// }
+
+// myFunction(a,b)
+
+// let a=10;
+// let b=20;
+// const myFunction=(a,b)=>{ //parameter
+//     console.log(a+b)
+//}
+// let a=10;
+// let b=20;
+// myFunction(a,b) //argument
+
+let fname="Rishita"
+let lname="Maity"
+console.log(fname,lname)
+
+let b=20,c="10"
+console.log(b+c)
